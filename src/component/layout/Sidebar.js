f0bf3fa6e82bac66b9fa17b143e6638693688089
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2021-01-30 02:35:38 +0530

    Change code in index & slidebar.js

--- src/component/layout/Sidebar.js
+++ src/component/layout/Sidebar.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import { FachevronDown,FaInbox,FaRegCalendarAlt,FaRegCalendar} from 'react-icons/fa'
 
-export const Sidebar = () =>
-       <p>I am sidebar</p>;
+export const Sidebar = () =>(
+       <div className="sidebar" data-testid ="sidebar">
+       <ul className ="sidebar__generic">
+              <li><span><FaInbox/></span><span>Inbox</span></li>
+              <li><span><FaRegCalendar/></span><span>Today</span></li>
+              <li><span><FaRegCalendarAlt/></span><span>Next 7 days</span></li>
+       </ul>
+</div>
+);
